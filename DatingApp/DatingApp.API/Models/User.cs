717750b8f999b823367dc07aac553e4dcd177239
Javier Hermoso Blanco <jhermoso@pretor.es>
2020-08-20 20:45:07 +0200

    .gitignore init

--- DatingApp/DatingApp.API/Models/User.cs
+++ DatingApp/DatingApp.API/Models/User.cs
@@ -11,5 +11,22 @@ namespace DatingApp.API.Models
         public string UserName  { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAS { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }    
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Photo> Phothos { get; set; }    
     }
 }
